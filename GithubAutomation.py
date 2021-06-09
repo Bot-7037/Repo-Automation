282e7736933cb2ca8d7f7ecf75cc46ed420dbe08
Bot-7037 <hnegi12345@gmail.com>
2021-06-09 22:38:09 +0530

    Improving TUI

--- GithubAutomation.py
+++ GithubAutomation.py
@@ -1,37 +1,110 @@
-import colored
+import curses
 import os
 from time import sleep
 from selenium import webdriver
-from stdiomask import getpass
 
-print(colored.stylize("\n---- | Create GitHub Repo | ----\n", colored.fg("red")))
+def CreateRepo(stdscr):
+    file = open('./src/credentials','r').read()
+    stdscr.clear()
+    username = file.split()[0]
+    password = file.split()[1]
 
-username = input("Username on GitHub: ")
+    stdscr.clear()
+    y, x = stdscr.getmaxyx()
+    _ = "Enter repository name"
+    repoName=""
+    stdscr.addstr(y//2, x//2 - len(_)//2, _)
+    stdscr.refresh()
 
-password = getpass(prompt = "Password on GitHub: ")
-repoName = input("Repository name: ")
-print()
+    while(1):
+        key = stdscr.getch()
+        if(key == curses.KEY_ENTER or key in [10,13]):
+            break
+        if(key == curses.KEY_BACKSPACE or key in [127, '\b', 8]):
+            if(len(repoName) <2):
+                repoName = ""
+            else:
+                repoName = repoName[:-1]
+            stdscr.clear()
+            stdscr.addstr(y//2, x//2 - len(_)//2, _)
+            stdscr.addstr(y//2+1, x//2 - len(repoName)//2, repoName)
+            stdscr.refresh()
+            continue
 
-class CreateRepo:
-    def __init__(self, username, password, repoName):
-        self.username = username
-        self.password = password
-        self.repoName = repoName
-        
-        self.driver = webdriver.Chrome(os.getcwd()+'\chromedriver.exe')
+        repoName+= chr(key)
+        stdscr.clear()
+        stdscr.addstr(y//2, x//2 - len(_)//2, _)
+        stdscr.addstr(y//2+1, x//2 - len(repoName)//2, repoName)
+        stdscr.refresh()
 
-        # Access GitHub Login Page
-        self.driver.get("https://github.com/new")
+    driver = webdriver.Chrome(os.getcwd()+'\chromedriver.exe')
 
-        # Log in to GitHub
-        self.driver.find_element_by_id("login_field").send_keys(self.username)
-        self.driver.find_element_by_id("password").send_keys(self.password)
-        self.driver.find_element_by_name("commit").click()
+    # Access GitHub Login Page
+    driver.get("https://github.com/new")
 
-        # Create New Repository
-        self.driver.find_element_by_id("repository_name").send_keys(self.repoName)
-        sleep(2)
-        self.driver.find_element_by_xpath("//*[@id='new_repository']/div[4]/button").click()
-        sleep(2)
+    # Log in to GitHub
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("login_field").send_keys(username)
+    driver.find_element_by_name("commit").click()
 
-CreateRepo(username, password, repoName)
+    # Create New Repository
+    driver.find_element_by_id("repository_name").send_keys(repoName)
+    driver.find_element_by_xpath("//*[@id='new_repository']/div[4]/button").click()
+
+    exit()
+
+def AddCredentials(stdscr):
+    stdscr.clear()
+    y, x = stdscr.getmaxyx()
+    _ = "Enter Username"
+    username=""
+    stdscr.addstr(y//2, x//2 - len(_)//2, _)
+    stdscr.refresh()
+
+    while(1):
+        key = stdscr.getch()
+        curses.curs_set(1)
+        if(key == curses.KEY_ENTER or key in [10,13]):
+            break
+        if(key == curses.KEY_BACKSPACE or key in [127, '\b', 8]):
+            if(len(username) <2):
+                username = ""
+            else:
+                username = username[:-1]
+            stdscr.clear()
+            stdscr.addstr(y//2, x//2 - len(_)//2, _)
+            stdscr.addstr(y//2+1, x//2 - len(username)//2, username)
+            stdscr.refresh()
+            continue
+        username+= chr(key)
+        stdscr.clear()
+        stdscr.addstr(y//2, x//2 - len(_)//2, _)
+        stdscr.addstr(y//2+1, x//2 - len(username)//2, username)
+        stdscr.refresh()
+    curses.curs_set(0)
+    username += ' '
+
+    stdscr.clear()
+    _ = "Enter Password"
+    password=""
+    stdscr.addstr(y//2, x//2 - len(_)//2, _)
+    stdscr.refresh()
+
+    while(1):
+        key = stdscr.getch()
+        if(key == curses.KEY_ENTER or key in [10,13]):
+            break
+        if(key == curses.KEY_BACKSPACE or key in [127, '\b', 8]):
+            if(len(password) <2):
+                password = ""
+            else:
+                password = password[:-1]
+            continue
+        username+= chr(key)
+        stdscr.addstr(y//2, x//2 - len(_)//2, _)
+        stdscr.refresh()
+    
+    file = open('./src/credentials', "w")
+    cred = username + password
+    file.write(cred)
+    file.close()
---
+++ Main.py
@@ -0,0 +1,51 @@
+from time import sleep
+from selenium import webdriver
+import GithubAutomation
+import curses
+import os
+
+options = [' Create New ', ' List Repos ', ' Delete Repo ', ' Add Credentials ', ' Exit ']
+
+def print_menu(stdscr, current_selection):
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    stdscr = curses.initscr()
+    y, x = stdscr.getmaxyx()
+    for idx, i in enumerate(options):
+        if(current_selection == idx+1):
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y//2+idx-3, x//2 -(len(i)//2), i)    
+            stdscr.attroff(curses.color_pair(1))
+            continue
+        stdscr.addstr(y//2+idx-3, x//2 -(len(i)//2), i)
+    stdscr.refresh()
+
+def main(stdscr):
+
+    stdscr = curses.initscr()
+    curses.curs_set(0)
+    current_selection = 1
+
+    y, x = stdscr.getmaxyx()
+
+    logo = open('./src/Logo').read()
+    for idx, i in enumerate(logo.split('\n')):
+        stdscr.addstr(idx+y//2-11, x//2-24, i)
+    stdscr.refresh()
+    sleep(1.5)
+    stdscr.clear()
+    
+    while(1):
+        print_menu(stdscr, current_selection)
+        key = stdscr.getch()
+        if(key == curses.KEY_UP and current_selection>1):
+            current_selection-=1
+        elif(key == curses.KEY_DOWN and current_selection<5):
+            current_selection+=1
+        elif(key == curses.KEY_ENTER or key in [10, 13]):
+            if(current_selection == 5):
+                exit()
+            if(current_selection == 1):
+                GithubAutomation.CreateRepo(stdscr)
+            if(current_selection == 4):
+                GithubAutomation.AddCredentials(stdscr)
+curses.wrapper(main)
